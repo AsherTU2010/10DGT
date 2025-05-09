8ecafe9e7540895a503e6f42f1baf90cd6acc148
AsherTU2010 <ashermt2010@gmail.com>
2025-05-09 14:11:06 +1200

    Add files via upload

--- inputs and loops.py
+++ inputs and loops.py
@@ -1,26 +1,26 @@
 # Inputs and Loops
 # Author: Asher Thompson
-# Date: 7/05/2025
-# Version 1.0
+# Date: 9/05/2025
+# Version 2.0
 
 # Ask the user a question and store the answer in a variable
 # Ask the user for their name and store it 
 name = input("Hey, What is your name? ")
 
 # Ask the user for two numbers and then add them together
-num1 = int(input("What is your first number? "))
+num1 = int(input("\nWhat is your first number? "))
 num2 = int(input("What is your second number? "))
-print(f"You first number was {num1}")
-print(f"You second number was {num2}")
-
-# To check that the input was stored correctly
-print(num1)
-print(num2)
+print(f"\nYour first number was {num1}")
+print(f"Your second number was {num2}")
 
 # Adding the input together
 sum = num1 + num2
-print(sum)
+print(f"The sum of the two numbers is {sum}\n")
 
-# Test that the input was stored correctly 
-print(name)
+# Multiply the input together
+product = num1 * num2
+print(f"The product of the two numbers is {product}\n")
+
+# Test that the input was stored correctly
+print(f"Nice to meet you {name}!")
 # to comment code out, use ctrl + /
